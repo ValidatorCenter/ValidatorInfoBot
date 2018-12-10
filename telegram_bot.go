ac79bc306f9df888eabbbd5f6959827d033287f1
dnk <dnk@k0d.su>
2018-12-10 23:20:27 +0300

    fix2

--- telegram_bot.go
+++ telegram_bot.go
@@ -76,38 +76,24 @@ type usrData struct {
 
 // структура кандидата/валидатора
 type candidate_info struct {
-	CandidateAddress string        `json:"candidate_address" bson:"candidate_address"`
-	TotalStake       string        `json:"total_stake" bson:"total_stake"`
-	TotalStake32     float32       `bson:"total_stake32"`
-	PubKey           string        `json:"pub_key" bson:"pub_key"`
-	Commission       int           `json:"commission" bson:"commission"`
-	CreatedAtBlock   int           `json:"created_at_block" bson:"created_at_block"`
-	StatusInt        int           `json:"status" bson:"status"` // числовое значение статуса: 1 - Offline, 2 - Online
-	Stakes           []stakes_info `json:"stakes" bson:"stakes"`
+	CandidateAddress string `json:"candidate_address" bson:"candidate_address"`
+	TotalStake       string `json:"total_stake" bson:"total_stake"`
+	PubKey           string `json:"pub_key" bson:"pub_key"`
+	Commission       int    `json:"commission" bson:"commission"`
+	CreatedAtBlock   int    `json:"created_at_block" bson:"created_at_block"`
+	StatusInt        int    `json:"status" bson:"status"` // числовое значение статуса: 1 - Offline, 2 - Online
+	//Stakes           []stakes_info `json:"stakes" bson:"stakes"`
 }
 
 // стэк делегатов
-type stakes_info struct {
+/*type stakes_info struct {
 	Owner      string  `json:"owner" bson:"owner"`
 	Coin       string  `json:"coin" bson:"coin"`
 	Value      string  `json:"value" bson:"value"`
 	BipValue   string  `json:"bip_value" bson:"bip_value"`
 	Value32    float32 `bson:"value32"`
 	BipValue32 float32 `bson:"bip_value32"`
-}
-
-// Перевод из строки в число с запятой
-func cnvStr2Float_18(amntTokenStr string) float32 {
-	var fAmntToken float32 = 0.0
-	if amntTokenStr != "" {
-		fAmntToken64, err := strconv.ParseFloat(amntTokenStr, 64)
-		if err != nil {
-			panic(err.Error())
-		}
-		fAmntToken = float32(fAmntToken64 / 1000000000000000000)
-	}
-	return fAmntToken
-}
+}*/
 
 // Статус мастерноды
 func getNodeStatusString(statInt int) string {
@@ -120,7 +106,11 @@ func getNodeStatusString(statInt int) string {
 
 // Сокращение строки
 func getMinString(bigStr string) string {
-	return fmt.Sprintf("%s...%s", bigStr[:6], bigStr[len(bigStr)-4:len(bigStr)])
+	if len(bigStr) > 8 {
+		return fmt.Sprintf("%s...%s", bigStr[:6], bigStr[len(bigStr)-4:len(bigStr)])
+	} else {
+		return bigStr
+	}
 }
 
 // Загрузка пользователей из БД в память
@@ -254,8 +244,8 @@ func ReturnValid() {
 		}
 		var data candidate_info
 		json.Unmarshal(body, &data)
-		//---
-		data.TotalStake32 = cnvStr2Float_18(data.TotalStake)
+
+		fmt.Fprintf("%#v\n", data) // TODO: скрыть
 
 		allValid = append(allValid, data)
 	}
@@ -455,7 +445,7 @@ func main() {
 					if oUsr.Notification == true {
 						chekIt = "да"
 					}
-					reply = fmt.Sprintf("Ключ: %s\nАдрес: %s\nПрив.ключ: %s\nСтатус: %s\nКомиссия: %d%%\nСтэк: %f\nОповещение: %s", getMinString(oUsr.PubKey), getMinString(oUsr.UserAddress), getMinString(oUsr.PrivKey), getNodeStatusString(cndI.StatusInt), cndI.Commission, cndI.TotalStake32, chekIt)
+					reply = fmt.Sprintf("Ключ: %s\nАдрес: %s\nПрив.ключ: %s\nСтатус: %s\nКомиссия: %d%%\nСтэк: %f\nОповещение: %s", getMinString(oUsr.PubKey), getMinString(oUsr.UserAddress), getMinString(oUsr.PrivKey), getNodeStatusString(cndI.StatusInt), cndI.Commission, cndI.TotalStake, chekIt)
 				} else {
 					if oUsr.ChatID == 0 {
 						reply = "Добавьте мастерноду для слежения, командой /node_add"
@@ -471,7 +461,7 @@ func main() {
 				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Найдено мастернод: %d", amntRes)))
 
 				for iN, oNd := range resSrch {
-					reply = fmt.Sprintf("= Мастернода %d ==========\nКлюч: %s\nСтатус: %s\nКомиссия: %d%%\nСтэк: %f", (iN + 1), oNd.PubKey, getNodeStatusString(oNd.StatusInt), oNd.Commission, oNd.TotalStake32)
+					reply = fmt.Sprintf("= Мастернода %d ==========\nКлюч: %s\nСтатус: %s\nКомиссия: %d%%\nСтэк: %f", (iN + 1), oNd.PubKey, getNodeStatusString(oNd.StatusInt), oNd.Commission, oNd.TotalStake)
 					msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 
 					btnKeyboard := tgbotapi.NewInlineKeyboardMarkup(
